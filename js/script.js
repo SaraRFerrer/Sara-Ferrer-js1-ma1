23047ae5a21011e2c742a1ed0faf6fc179aaf92c
SaraRFerrer <sararferrer@hotmail.com>
2022-01-28 13:04:08 +0100

    save

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Question 1
+
+const cat = {
+    complain: function() {
+        console.log("Meow");
+    }
+    
+}
+
+// Question 2 and 3 and 4
+
+const heading = document.querySelector("h3"); 
+heading.innerHTML = "Updated Heading";
+heading.style.fontSize = "2em";
+
+heading.classList.add("subheading");
+
+
