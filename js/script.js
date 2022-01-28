81a4a7c9daa75ec6fad563c9d26eaa2dfe061a37
SaraRFerrer <sararferrer@hotmail.com>
2022-01-28 13:55:14 +0100

    save

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ heading.style.fontSize = "2em";
 heading.classList.add("subheading");
 
 
+// Question 5 
+
+const paragraphs = document.querySelectorAll("p");
+
+console.dir(paragraphs);
+
+for(let i = 0; i < paragraphs.length; i++) {
+    console.dir(paragraphs[i].innerHTML);
+    paragraphs[i].style.color = "red";
+}
+
+
+// Question 6 
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p>New paragraph</p>";
+resultsContainer.style.backgroundColor = "yellow";
+
